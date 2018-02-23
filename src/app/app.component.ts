91331f66b65f99169561bbc4c6b2187a2468f808
Wesley Wu <wewu@castlighthealth.com>
2018-02-22 20:59:37 -0800

    testing API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  testApi() {
+    this.http.get("/api/contacts").subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
