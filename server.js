fa4dc616236da5db84b328b1231187ae47b47c90
Jake Gilger <jgilger@castlighthealth.com>
2018-02-23 15:18:53 -0800

    default location for running mongo locally

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(distDir));
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/atiLocalDb', function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
